7fa66a0394f8a60679ce7be8dc65f163aa8372d0
Ius <timotiushadipratama@gmail.com>
2021-06-25 13:36:49 +0700

    Update alarmData.php

--- sumberdayafile/alarmData.php
+++ sumberdayafile/alarmData.php
@@ -2,19 +2,19 @@
 define("MYSQL_DB_HOST", "localhost");
 define("MYSQL_DB_USER", "admin");
 define("MYSQL_DB_PASSWORD", "angelus25");
-define("MYSQL_DB_DATABASE", "dataPenguna");
+define("MYSQL_DB_DATABASE", "dataPengguna");
 $conn = mysqli_connect(MYSQL_DB_HOST, MYSQL_DB_USER, MYSQL_DB_PASSWORD, MYSQL_DB_DATABASE);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$jamStart = mysqli_real_escape_string($conn, $_POST['startHour']);
-$jamBerakhir = mysqli_real_escape_string($conn, $_POST['endHour']);
+$startHour = mysqli_real_escape_string($conn, $_POST['startHour']);
+$endHour = mysqli_real_escape_string($conn, $_POST['endHour']);
 $intervalWaktu = mysqli_real_escape_string($conn, $_POST['intervalWaktu']);
 
-if(!empty($user)&&!empty($email)&&!empty($ponsel)&&!empty($password)){
-	$sql = "INSERT INTO dataWaktuTimer(jamStart, jamBerakhir, intervalWaktu) VALUES ('{$jamStart}', '{$jamBerakhir}', '{$intervalWaktu}');";
-	$query = mysqli_query($conn, $sql) or die("alarm sudah ada");
+if(!empty($startHour)&&!empty($endHour)&&!empty($intervalWaktu)){
+	$sql = "INSERT INTO dataWaktuTimer(jamStart, jamBerakhir, intervalWaktu) VALUES ('{$startHour}', '{$endHour}', '{$intervalWaktu}');";
+	$query = mysqli_query($conn, $sql) or die("Alarm sudah ada");
 	die("OK");
 }
 else {
